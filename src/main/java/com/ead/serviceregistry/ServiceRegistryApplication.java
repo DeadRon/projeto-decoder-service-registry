8ac09ecdcd82d659230fb07ed20cd106875b87e7
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-01-28 01:12:41 -0300

    [CONFIG] - Habilita a aplicação como servidor Eureka

--- src/main/java/com/ead/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/ead/serviceregistry/ServiceRegistryApplication.java
@@ -2,8 +2,11 @@ package com.ead.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//Habilita a aplicação como servidor Eureka
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
